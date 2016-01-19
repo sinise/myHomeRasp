5c17f02bc736328e44ca019b17298072db1f23c8
seastian <sebastian@momentos.dk>
2016-01-19 11:53:25 +0100

    getting temp from file working

--- nodeApp/pid.js
+++ nodeApp/pid.js
@@ -1,6 +1,9 @@
 var liquidPID = require('liquid-pid'),
     actualP = 0,
-    pidController;
+    pidController,
+    fs = require('fs'),
+    sleep = require('sleep'),
+    http = require('http');
 
 pidController = new liquidPID({
   temp: {
@@ -14,13 +17,40 @@ pidController = new liquidPID({
   Kd: 9             // PID: Kd
 });
 
-while (true){
-actualP = pidController.calculate(66.7);	// call with the actual temp
-console.log(actualP);
 
-actualP = pidController.calculate(67.0);    // call with the actual temp
-console.log(actualP);
+
+
+
+var getTemp = function(path, cb){
+  fs.readFile(path, 'utf8', function (err, data) {
+    if (err){
+      console.log(err);
+      throw err;
+    }
+    var temp = data.match(/YES|[0-9]{5}/g)      
+    var tempObj = {temp: temp[1]/1000, err: temp[0]};
+      console.log(tempObj.temp);
+    cb(tempObj);
+  });
+  
+
+}
+
+
+
 
 actualP = pidController.calculate(69.0);    // call with the actual temp
 console.log(actualP);
-}
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+  console.log('send http');
+  getTemp('./w1_slave',function(temp){
+    console.log('im callback' + temp.temp)
+    actualP = pidController.calculate(temp.temp);    // call with the actual temp
+    console.log(actualP);
+
+  })
+
+}).listen(3000);
