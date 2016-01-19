62c9ee41ab5c298204b8d2d32d997487a4c057f9
seastian <sebastian@momentos.dk>
2016-01-19 13:37:40 +0100

    pid working but not tuned

--- nodeApp/pid.js
+++ nodeApp/pid.js
@@ -1,25 +1,30 @@
-var liquidPID = require('liquid-pid'),
-    actualP = 0,
-    pidController,
-    fs = require('fs'),
+var fs = require('fs'),
     sleep = require('sleep'),
-    http = require('http');
+    http = require('http'),
+    PID = require('pid-controller');
 
-pidController = new liquidPID({
-  temp: {
-    ref: 67         // Point temperature                                       
-  },
-  Pmax: 1000,       // Max power (output),
-  
-  // Tune the PID Controller
-  Kp: 25,           // PID: Kp
-  Ki: 1000,         // PID: Ki
-  Kd: 9             // PID: Kd
-});
+var temperture = 20,
+    tempertureSetpoint = 23;
 
+var Kp = 50,
+    Ki = 1,
+    Kd = 0;
 
+var ctr = new PID(temperture, tempertureSetpoint, Kp, Ki, Kd, 'direct'),
+    timeframe = 1000;
 
-
+ctr.setSampleTime(timeframe);
+ctr.setOutputLimits(0, timeframe);
+ctr.setMode('auto');
+var calculatePid = function(temp) {
+    if (typeof calculatePid.counter == 'undefined') {
+        calculatePid.counter = 0;
+    }
+    ctr.setInput(temperture);
+    ctr.setMode(1);
+    ctr.compute();
+    console.log("Output : " + ctr.getOutput() + " ; Temp : " + ctr.getInput() + "°c");
+};
 
 var getTemp = function(path, cb){
   fs.readFile(path, 'utf8', function (err, data) {
@@ -29,28 +34,20 @@ var getTemp = function(path, cb){
     }
     var temp = data.match(/YES|[0-9]{5}/g)      
     var tempObj = {temp: temp[1]/1000, err: temp[0]};
-      console.log(tempObj.temp);
-    cb(tempObj);
+    temperture = tempObj.temp;
+    cb(tempObj.temp);
   });
-  
-
 }
 
-
-
-
-actualP = pidController.calculate(69.0);    // call with the actual temp
-console.log(actualP);
+function loop(){getTemp('./w1_slave', calculatePid)}
+setInterval(loop, timeframe);
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
   console.log('send http');
-  getTemp('./w1_slave',function(temp){
-    console.log('im callback' + temp.temp)
-    actualP = pidController.calculate(temp.temp);    // call with the actual temp
-    console.log(actualP);
+//  getTemp('./w1_slave',function(temp){
+ //   console.log('im callback' + temp.temp)
 
-  })
-
+  //})
 }).listen(3000);
